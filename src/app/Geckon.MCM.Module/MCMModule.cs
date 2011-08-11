5c2f746a93e6bd173aaec7f0927674c83995634a
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2011-08-11 16:33:25 +0200

    Added: MCM .Net Solution

---
+++ src/app/Geckon.MCM.Module/MCMModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using Geckon.Portal.Core.Standard.Module;
+
+namespace Geckon.MCM.Module
+{
+    public class MCMModule : AModule
+    {
+        #region Properties
+
+        private String ConnectionString { get; set; }
+
+        #endregion
+        #region Construction
+
+        public override void Init( XElement config )
+        {
+            ConnectionString = config.Descendants( "ConnectionString" ).First().Value;
+        }
+
+        #endregion
+        #region MyRegion
+        
+
+
+        #endregion
+    }
+}
