e2d0ffc74097d167aa07f3aff888abbcc2dfa5c6
fyhr <jesper@fyhr.dk>
2011-11-18 10:11:39 +0100

    Modified: Fixed bug in Metadata_Set

--- sql/changes.sql
+++ sql/changes.sql
@@ -161,3 +161,71 @@ BEGIN
 
 END
 GO
+
+-- =============================================
+-- Author:		Jesper Fyhr Knudsen
+-- Create date: 2011.10.04
+--				This SP creates or updates metadata
+-- =============================================
+ALTER PROCEDURE [dbo].[Metadata_Set]
+	@GroupGUIDs		    GUIDList Readonly,
+	@UserGUID		    uniqueidentifier,
+	@ObjectGUID			uniqueidentifier,
+	@MetadataSchemaGUID uniqueidentifier,
+	@LanguageCode		varchar(10),
+	@MetadataXML		xml,
+	@Lock				bit = NULL
+AS
+BEGIN
+	
+	DECLARE @ObjectID         INT
+	DECLARE @MetadataSchemaID INT
+	
+	SELECT	@ObjectID = ID
+	  FROM	[Object]
+	 WHERE	[GUID] = @ObjectGUID
+	 
+	SELECT	@MetadataSchemaID = ID
+	  FROM	MetadataSchema
+	 WHERE	[GUID] = @MetadataSchemaGUID
+	
+	DECLARE	@RequiredPermission	int
+	SET @RequiredPermission = dbo.GetPermissionForAction( 'Metadata', 'CREATE_UPDATE_OBJECTS' )
+
+	IF( @RequiredPermission & dbo.Object_FindHighestUserPermission( @UserGUID,@GroupGUIDs,@ObjectID ) <> @RequiredPermission )
+			RETURN -100
+	
+	IF EXISTS( SELECT ID FROM Metadata WHERE ObjectID = @ObjectID AND MetadataSchemaID = @MetadataSchemaID AND LanguageCode = @LanguageCode )
+	BEGIN
+	
+		DECLARE @DateLocked	DATETIME
+		DECLARE @LockUserGUID UNIQUEIDENTIFIER
+	
+		SELECT	@DateLocked = GETDATE(),
+				@LockUserGUID = @UserGUID
+		 WHERE	@Lock = 1
+	
+		UPDATE [Metadata]
+		   SET [MetadataXml]  = @MetadataXML,
+		       [DateModified] = GETDATE(),
+		       [DateLocked]   = ISNULL(@DateLocked,[DateLocked]),
+		       [LockUserGUID] = ISNULL(@LockUserGUID,[LockUserGUID])
+		WHERE  ObjectID         = @ObjectID AND 
+			   MetadataSchemaID = @MetadataSchemaID AND 
+			   LanguageCode     = @LanguageCode
+	
+		RETURN @@ROWCOUNT
+	
+	END
+	ELSE 
+	BEGIN
+			
+		INSERT INTO [Metadata]([ObjectID],LanguageCode,[MetadataSchemaID],[MetadataXml],[DateCreated],[DateModified],[DateLocked],[LockUserGUID])
+			 VALUES (@ObjectID,@LanguageCode,@MetadataSchemaID,@MetadataXML,GETDATE(),GETDATE(),null,null )
+
+		RETURN @@ROWCOUNT
+		
+	END
+END
+GO
+
