fc86a1ea407cb6a95911da1a340db065b2cebd47
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-09-25 18:54:26 +0200

    Fixed issue where indexing would fail if fiat dates were missing

--- src/app/CHAOS.MCM.Data.DTO/Object.cs
+++ src/app/CHAOS.MCM.Data.DTO/Object.cs
@@ -142,14 +142,14 @@ namespace CHAOS.MCM.Data.DTO
 
                         // FIAT / IFTA
                         case "22c70550-90ce-43f9-9176-973c09760138":
-                            if (metadata.MetadataXML.Root.Element("FirstPublicationDate") != null)
+                            if (!string.IsNullOrEmpty(metadata.MetadataXML.Root.Element("FirstPublicationDate").Value))
                                 yield return new KeyValuePair<string, string>("FIATIFTA-ANP-FirstPublicationDate", 
                                     DateTime.Parse(metadata.MetadataXML.Root.Element("FirstPublicationDate").Value).ToString( "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'" )  );
 
-                            if (metadata.MetadataXML.Root.Element("Title") != null)
+                            if (!string.IsNullOrEmpty(metadata.MetadataXML.Root.Element("Title").Value))
                                 yield return new KeyValuePair<string, string>("FIATIFTA-ANP-Title", metadata.MetadataXML.Root.Element("Title").Value);
 
-                            if (metadata.MetadataXML.Root.Element("Publisher") != null)
+                            if (!string.IsNullOrEmpty(metadata.MetadataXML.Root.Element("Publisher").Value))
                                 yield return new KeyValuePair<string, string>("FIATIFTA-ANP-Publisher", metadata.MetadataXML.Root.Element("Publisher").Value);
                             break;
 
