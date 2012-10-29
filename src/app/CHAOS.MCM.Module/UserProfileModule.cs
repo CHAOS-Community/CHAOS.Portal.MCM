93e6345a50527b30eaba0fc3ceeb246f6a219ebd
Jacob Poul Richardt <jacob@geckon.com>
2012-10-29 14:38:54 +0100

    Added UserProfileModule.

--- src/app/CHAOS.MCM.Module/UserProfileModule.cs
+++ src/app/CHAOS.MCM.Module/UserProfileModule.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 using CHAOS.MCM.Data.DTO;
 using CHAOS.Portal.Core;
 using CHAOS.Portal.Core.Module;
@@ -11,21 +14,32 @@ namespace CHAOS.MCM.Module
 	public class UserProfileModule : AMCMModule
 	{
 		[Datatype("UserProfile", "Get")]
-		public IEnumerable<Metadata> Get(ICallContext callContext, UUID userGUID, UUID schemaGUID)
+		public IEnumerable<Metadata> Get(ICallContext callContext, UUID userGUID, UUID metadataSchemaGUID)
 		{
 			if (callContext.IsAnonymousUser)
 				throw new InsufficientPermissionsException("User must be logged in");
 
-
+			throw new NotImplementedException();
 		}
 
 		[Datatype("UserProfile", "Set")]
-		public ScalarResult Set(ICallContext callContext, UUID userGUID, UUID schemaGUID, string profileXML)
+		public ScalarResult Set(ICallContext callContext, UUID metadataSchemaGUID, string profileXML)
 		{
 			if (callContext.IsAnonymousUser)
 				throw new InsufficientPermissionsException("User must be logged in");
 
+			XDocument.Parse( profileXML ); //TODO: Real xml validation against schema
 
+			throw new NotImplementedException();
+
+			/*
+			using (var db = DefaultMCMEntities)
+			{
+				var result = db.Metadata_Set(new UUID().ToByteArray(), callContext.User.GUID.ToByteArray(), metadataSchemaGUID.ToByteArray(), null, (int?)revisionID, profileXML, callContext.User.GUID.ToByteArray()).First().Value;
+
+				if (result <= 0)
+					throw new UnhandledException("UserProfile Set was rolledback due to an unhandled exception");
+			}*/
 
 			return new ScalarResult(1);
 		}
