1e26f13f73e6abaed0f1a97982115476d3470b31
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2013-02-01 18:47:09 +0100

    Added Version table to the database, to keep track of which scripts has been run Fixed a bug in the Object_Metadata_Join create script

--- sql/1.baseline/tables/4.Object_Metadata_Join.sql
+++ sql/1.baseline/tables/4.Object_Metadata_Join.sql
@@ -1,5 +1,9 @@
-CREATE  TABLE Object_Metadata_Join(	ObjectGuid		BINARY(16) NOT NULL,	MetadataGuid	BINARY(16) NOT NULL,
-	PRIMARY KEY (ObjectGuid, MetadataGuid) 
+CREATE  TABLE Object_Metadata_Join
+(
+	ObjectGuid		BINARY(16) NOT NULL,
+	MetadataGuid	BINARY(16) NOT NULL,
+
+	PRIMARY KEY (ObjectGuid, MetadataGuid),
 	KEY fk_Object_Metadata_Join_Metadata_MetadataGuid_idx (MetadataGuid),
 	KEY fk_Object_Metadata_Join_Object_ObjectGuid_idx (ObjectGuid),
 	CONSTRAINT fk_Object_Metadata_Join_Metadata_MetadataGuid FOREIGN KEY (MetadataGuid) REFERENCES Metadata (GUID),
