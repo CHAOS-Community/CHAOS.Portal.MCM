9b2970539a4da20f91dc1e4b8b9b3c4d473b42d0
Jacob Poul Richardt <jacob@geckon.com>
2013-02-06 13:01:19 +0100

    Moved MCM "types" extensions to separate files.

---
+++ src/app/CHAOS.MCM/Extension/FormatType.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Chaos.Mcm.Data.EF;
+using Chaos.Portal;
+
+namespace Chaos.Mcm.Extension
+{
+	public class FormatType : AMcmExtension
+	{
+		public IEnumerable<Data.Dto.Standard.FormatType> Get(ICallContext callContext, string name)
+		{
+			using (MCMEntities db = DefaultMCMEntities)
+			{
+				return db.FormatType_Get(null, name).ToDto().ToList();
+			}
+		}
+
+		//[Datatype("FormatType", "Create")]
+		//public FormatType FormatType_Create(CallContext callContext, string name)
+		//{
+		//    using( MCMEntities db = DefaultMCMEntities )
+		//    {
+		//        int result = db.FormatType_Create(name, callContext.User.SystemPermission);
+
+		//        if( result == -100 )
+		//            throw new Portal.Core.Exception.InsufficientPermissionsException("User does not have permission to delete an Object Type");
+
+		//        return db.FormatType_Get(result, null).First();
+		//    }
+		//}
+
+		//[Datatype("FormatType", "Update")]
+		//public ScalarResult FormatType_Update(CallContext callContext, int? id, string name)
+		//{
+		//    using( MCMEntities db = DefaultMCMEntities )
+		//    {
+		//        int result = db.FormatType_Update( id, name, callContext.User.SystemPermission );
+
+		//        if( result == -100 )
+		//            throw new Portal.Core.Exception.InsufficientPermissionsException("User does not have permission to delete an Object Type");
+
+		//        return new ScalarResult(result);
+		//    }
+		//}
+
+		//[Datatype("FormatType", "Delete")]
+		//public ScalarResult FormatType_Delete(CallContext callContext, int? id)
+		//{
+		//    using( MCMEntities db = DefaultMCMEntities )
+		//    {
+		//        int result = db.FormatType_Delete(id, callContext.User.SystemPermission);
+
+		//        if( result == -100 )
+		//            throw new Portal.Core.Exception.InsufficientPermissionsException("User does not have permission to delete an Object Type");
+
+		//        return new ScalarResult(result);
+		//    }
+		//}
+	}
+}
