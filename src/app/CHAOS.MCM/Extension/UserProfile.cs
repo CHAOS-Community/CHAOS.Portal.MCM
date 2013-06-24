c0bb4e8fb231477d1a754c69b4e4195e6c60aeaf
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-06-24 04:57:27 +0200

    Implemented UserProfile/Set.

--- src/app/CHAOS.MCM/Extension/UserProfile.cs
+++ src/app/CHAOS.MCM/Extension/UserProfile.cs
@@ -6,6 +6,7 @@ using Chaos.Mcm.Permission;
 using Chaos.Portal.Core;
 using Chaos.Portal.Core.Data.Model;
 using System.Linq;
+using CHAOS.Extensions;
 
 namespace Chaos.Mcm.Extension
 {
@@ -39,9 +40,24 @@ namespace Chaos.Mcm.Extension
 		#endregion
 		#region Get
 
-		public ScalarResult Set(XDocument metadata, Guid metadataSchemaGuid, Guid? userGuid = null)
+		public ScalarResult Set(Guid metadataSchemaGuid, XDocument metadata, Guid? userGuid = null)
 		{
-			throw new NotImplementedException();
+			if (!userGuid.HasValue)
+				userGuid = Request.User.Guid;
+
+			var userObject = McmRepository.ObjectGet(userGuid.Value, true);
+
+			if(userObject == null)
+				throw new NotImplementedException("User does not have an user object");
+
+			var existingMetadata = userObject.Metadatas.DoIfIsNotNull( ms => ms.FirstOrDefault(m => m.MetadataSchemaGuid == metadataSchemaGuid));
+			var metadataGuid = existingMetadata == null ? Guid.NewGuid() : existingMetadata.Guid;
+			var revision = existingMetadata == null ? 0 : existingMetadata.RevisionID;
+
+			if(McmRepository.MetadataSet(userObject.Guid, metadataGuid, metadataSchemaGuid, null, revision, metadata, Request.User.Guid) != 1)
+				throw new System.Exception("Failed to set user profile");
+
+			return new ScalarResult(1);
 		}
 
 		#endregion
--- src/test/CHAOS.MCM.Test/Extension/UserProfileTest.cs
+++ src/test/CHAOS.MCM.Test/Extension/UserProfileTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Xml.Linq;
 using Chaos.Mcm.Data.Dto;
+using Moq;
 using NUnit.Framework;
 
 namespace Chaos.Mcm.Test.Extension
@@ -33,6 +34,84 @@ namespace Chaos.Mcm.Test.Extension
 			
 			
 			Assert.That(result.MetadataXml, Is.EqualTo(expected.MetadataXml));
+		}
+
+		[Test]
+		public void Get_UserDoesNotHaveProfile_ReturnNull()
+		{
+			var extension = Make_UserProfileExtension();
+			var schemaGuid = Guid.NewGuid();
+			var user = SetupUser();
+
+			var userObject = Make_Object();
+			userObject.Guid = user.Guid;
+			userObject.Metadatas = new List<Metadata>
+				{
+					Make_MetadataDto()
+				};
+
+			McmRepository.Setup(m => m.ObjectGet(user.Guid, true, false, false, false, false)).Returns(userObject);
+
+			var result = extension.Get(schemaGuid);
+
+
+			Assert.That(result, Is.Null);
+		}
+
+		[Test]
+		public void Set_UserHasUserObjectButNoMetadata_SetUserMetadata()
+		{
+			var extension = Make_UserProfileExtension();
+			var schemaGuid = Guid.NewGuid();
+			var user = SetupUser();
+			var expected = Make_MetadataDto();
+			expected.MetadataSchemaGuid = schemaGuid;
+			expected.MetadataXml = XDocument.Parse("<UserProfil Name=\"Ben Dover\"/>");
+
+			var userObject = Make_Object();
+			userObject.Guid = user.Guid;
+			userObject.Metadatas = new List<Metadata>
+				{
+					Make_MetadataDto()
+				};
+
+			McmRepository.Setup(m => m.ObjectGet(user.Guid, true, false, false, false, false)).Returns(userObject);
+			McmRepository.Setup(m => m.MetadataSet(userObject.Guid, It.IsAny<Guid>(), schemaGuid, null, 0, expected.MetadataXml, user.Guid)).Returns(1);
+
+			var result = extension.Set(schemaGuid, expected.MetadataXml);
+
+			McmRepository.Verify(m => m.MetadataSet(userObject.Guid, It.IsAny<Guid>(), schemaGuid, null, 0, expected.MetadataXml, user.Guid));
+
+			Assert.That(result.Value, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Set_UserHasUserObjectAndMetadata_SetUserMetadata()
+		{
+			var extension = Make_UserProfileExtension();
+			var schemaGuid = Guid.NewGuid();
+			var user = SetupUser();
+			var expected = Make_MetadataDto();
+			expected.MetadataSchemaGuid = schemaGuid;
+			expected.MetadataXml = XDocument.Parse("<UserProfil Name=\"Ben Dover\"/>");
+			expected.RevisionID = 5;
+
+			var userObject = Make_Object();
+			userObject.Guid = user.Guid;
+			userObject.Metadatas = new List<Metadata>
+				{
+					Make_MetadataDto(),
+					expected
+				};
+
+			McmRepository.Setup(m => m.ObjectGet(user.Guid, true, false, false, false, false)).Returns(userObject);
+			McmRepository.Setup(m => m.MetadataSet(userObject.Guid, It.IsAny<Guid>(), schemaGuid, null, 5, expected.MetadataXml, user.Guid)).Returns(1);
+
+			var result = extension.Set(schemaGuid, expected.MetadataXml);
+
+			McmRepository.Verify(m => m.MetadataSet(userObject.Guid, It.IsAny<Guid>(), schemaGuid, null, 5, expected.MetadataXml, user.Guid));
+
+			Assert.That(result.Value, Is.EqualTo(1));
 		}
 	}
 }
