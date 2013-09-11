b25601edb1a368e2ae733acea1cb262d38d82f7b
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-09-11 19:49:14 +0200

    Updated Portal. Updated EmailService extension methods.

--- src/app/CHAOS.MCM/EmailServiceExtensions.cs
+++ src/app/CHAOS.MCM/EmailServiceExtensions.cs
@@ -1,32 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Xml.Linq;
-using Chaos.Mcm.Data;
-using Chaos.Portal.Core.EmailService;
-
-namespace Chaos.Mcm
+﻿namespace Chaos.Mcm
 {
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Xml.Linq;
+	using Data;
+	using Data.Dto;
+	using Portal.Core.EmailService;
+
 	public static class EmailServiceExtensions
 	{
-		public static void SendWithXSLT(this IEmailService emailService, string from, string to, string subject, IMcmRepository repository, Guid templateObjectGuid, Guid templateMetadataSchemaGuid, string templateLanguageCode, string data)
+		public static void SendTemplate(this IEmailService emailService, string from, string to, string subject, IMcmRepository repository, MetadataIdentifier template, XElement data)
 		{
-			emailService.SendWithXSLT(from, new List<string> { to }, subject, repository, templateObjectGuid, templateMetadataSchemaGuid, templateLanguageCode, data);
+			emailService.SendTemplate(from, new List<string> { to }, null, subject, repository, template, data);
 		}
 
-		public static void SendWithXSLT(this IEmailService emailService, string from, IEnumerable<string> to, string subject, IMcmRepository repository, Guid templateObjectGuid, Guid templateMetadataSchemaGuid, string templateLanguageCode, string data)
+		public static void SendTemplate(this IEmailService emailService, string from, IEnumerable<string> to, IEnumerable<string> bcc, string subject, IMcmRepository repository, MetadataIdentifier template, XElement data)
 		{
-			var templateObject = repository.ObjectGet(templateObjectGuid, true);
+			var templateMetadata = GetMetadata(repository, template);
 
-			if(templateObject == null || templateObject.Metadatas == null || templateObject.Metadatas.Count == 0)
-				throw new System.Exception(string.Format("Mail template not found. ObjectGuid: {0}", templateObjectGuid));
+			if (templateMetadata == null)
+				throw new System.Exception(string.Format("Failed to get template. ObjectGuid: {0} SchemaGuid: {1} LanguageCode: {2}", template.ObjectGuid, template.MetadataSchemaGuid, template.LanguageCode));
 
-			var templateMetadata = templateObject.Metadatas.FirstOrDefault(m => m.MetadataSchemaGuid == templateMetadataSchemaGuid && m.LanguageCode == templateLanguageCode);
+			emailService.SendTemplate(from, to, bcc, subject, templateMetadata, data);
+		}
 
-			if(templateMetadata == null)
-				throw new System.Exception(string.Format("Mail template not found on object. ObjectGuid: {0} SchemaGuid: {1} LanguageCode: {2}", templateObjectGuid, templateMetadataSchemaGuid, templateLanguageCode));
+		public static void SendTemplate(this IEmailService emailService, string from, string to, string subject, IMcmRepository repository, MetadataIdentifier template, IList<MetadataIdentifier> datas)
+		{
+			emailService.SendTemplate(from, new List<string> { to }, null, subject, repository, template, datas);
+		}
 
-			emailService.SendWithXSLT(from, to, subject, templateMetadata.MetadataXml.ToString(SaveOptions.DisableFormatting), data);
+		public static void SendTemplate(this IEmailService emailService, string from, IEnumerable<string> to, IEnumerable<string> bcc, string subject, IMcmRepository repository, MetadataIdentifier template, IList<MetadataIdentifier> datas)
+		{
+			var templateMetadata = GetMetadata(repository, template);
+
+			if (templateMetadata == null)
+				throw new System.Exception(string.Format("Failed to get template. ObjectGuid: {0} SchemaGuid: {1} LanguageCode: {2}", template.ObjectGuid, template.MetadataSchemaGuid, template.LanguageCode));
+
+			var dataMetadatas = new List<XElement>();
+
+			foreach (var metadataIdentifier in datas)
+			{
+				var metadata = GetMetadata(repository, metadataIdentifier);
+
+				if(metadata == null)
+					throw new System.Exception(string.Format("Failed to data. ObjectGuid: {0} SchemaGuid: {1} LanguageCode: {2}", metadataIdentifier.ObjectGuid, metadataIdentifier.MetadataSchemaGuid, metadataIdentifier.LanguageCode));
+
+				dataMetadatas.Add(metadata);
+			}
+
+			emailService.SendTemplate(from, to, bcc, subject, templateMetadata, dataMetadatas);
+		}
+
+		private static XElement GetMetadata(IMcmRepository repository, MetadataIdentifier metadata)
+		{
+			var templateObject = repository.ObjectGet(metadata.ObjectGuid, true);
+
+			if (templateObject == null || templateObject.Metadatas == null || templateObject.Metadatas.Count == 0)
+				return null;
+
+			var templateMetadata = templateObject.Metadatas.FirstOrDefault(m => m.MetadataSchemaGuid == metadata.MetadataSchemaGuid && m.LanguageCode == metadata.LanguageCode);
+
+			if (templateMetadata == null)
+				return null;
+
+			return templateMetadata.MetadataXml.Root;
 		}
 	}
 }
