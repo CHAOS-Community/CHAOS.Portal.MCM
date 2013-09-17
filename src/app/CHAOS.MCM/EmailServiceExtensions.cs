bcad889da4531b82f96edf225678a4bbba31a733
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-09-17 15:09:08 +0200

    Improved error feedback on email service.

--- src/app/CHAOS.MCM/EmailServiceExtensions.cs
+++ src/app/CHAOS.MCM/EmailServiceExtensions.cs
@@ -18,10 +18,7 @@
 
 		public static void SendTemplate(this IEmailService emailService, string from, IEnumerable<string> to, IEnumerable<string> bcc, string subject, IMcmRepository repository, MetadataIdentifier template, XElement data)
 		{
-			var templateMetadata = GetMetadata(repository, template);
-
-			if (templateMetadata == null)
-				throw new System.Exception(string.Format("Failed to get template. ObjectGuid: {0} SchemaGuid: {1} LanguageCode: {2}", template.ObjectGuid, template.MetadataSchemaGuid, template.LanguageCode));
+			var templateMetadata = GetMetadata(repository, template, "Failed to get template");
 
 			emailService.SendTemplate(from, to, bcc, subject, templateMetadata, data);
 		}
@@ -33,10 +30,7 @@
 
 		public static void SendTemplate(this IEmailService emailService, string from, IEnumerable<string> to, IEnumerable<string> bcc, string subject, IMcmRepository repository, MetadataIdentifier template, IList<MetadataIdentifier> datas)
 		{
-			var templateMetadata = GetMetadata(repository, template);
-
-			if (templateMetadata == null)
-				throw new System.Exception(string.Format("Failed to get template. ObjectGuid: {0} SchemaGuid: {1} LanguageCode: {2}", template.ObjectGuid, template.MetadataSchemaGuid, template.LanguageCode));
+			var templateMetadata = GetMetadata(repository, template, "Failed to get template");
 
 			var dataMetadatas = GetDatas(repository, datas);
 
@@ -94,44 +88,32 @@
 		#endregion
 		#region Utility
 
-		private static XElement GetMetadata(IMcmRepository repository, MetadataIdentifier metadata)
+		private static XElement GetMetadata(IMcmRepository repository, MetadataIdentifier metadata, string errorMessage = "Failed to get metadata")
 		{
 			var templateObject = repository.ObjectGet(metadata.ObjectGuid, true);
 
-			if (templateObject == null || templateObject.Metadatas == null || templateObject.Metadatas.Count == 0)
-				return null;
+			if(templateObject == null)
+				throw new System.Exception(string.Format("{0}, object not found. ObjectGuid: {1}", errorMessage, metadata.ObjectGuid));
+
+			if (templateObject.Metadatas == null || templateObject.Metadatas.Count == 0)
+				throw new System.Exception(string.Format("{0}, object has no metadata. ObjectGuid: {1}", errorMessage, metadata.ObjectGuid));
 
 			var templateMetadata = templateObject.Metadatas.FirstOrDefault(m => m.MetadataSchemaGuid == metadata.MetadataSchemaGuid && m.LanguageCode == metadata.LanguageCode);
 
 			if (templateMetadata == null)
-				return null;
+				throw new System.Exception(string.Format("{0}, no matching schema and language code. ObjectGuid: {1} SchemaGuid: {2} LanguageCode: {3}", errorMessage, metadata.ObjectGuid, metadata.MetadataSchemaGuid, metadata.LanguageCode));
 
 			return templateMetadata.MetadataXml.Root;
 		}
 
 		private static IList<XElement> GetDatas(IMcmRepository repository, IEnumerable<MetadataIdentifier> metadatas)
 		{
-			var dataMetadatas = new List<XElement>();
-
-			foreach (var metadataIdentifier in metadatas)
-			{
-				var metadata = GetMetadata(repository, metadataIdentifier);
-
-				if (metadata == null)
-					throw new System.Exception(string.Format("Failed to data. ObjectGuid: {0} SchemaGuid: {1} LanguageCode: {2}", metadataIdentifier.ObjectGuid, metadataIdentifier.MetadataSchemaGuid, metadataIdentifier.LanguageCode));
-
-				dataMetadatas.Add(metadata);
-			}
-
-			return dataMetadatas;
+			return metadatas.Select(metadataIdentifier => GetMetadata(repository, metadataIdentifier, "Failed to data")).ToList();
 		}
 
 		private static EmailTemplate GetEmailTemplate(IMcmRepository repository, MetadataIdentifier template)
 		{
-			var templateMetadata = GetMetadata(repository, template);
-
-			if (templateMetadata == null)
-				throw new System.Exception(string.Format("Failed to get template. ObjectGuid: {0} SchemaGuid: {1} LanguageCode: {2}", template.ObjectGuid, template.MetadataSchemaGuid, template.LanguageCode));
+			var templateMetadata = GetMetadata(repository, template, "Failed to get template");
 
 			var from = templateMetadata.Element("From");
 			var subject = templateMetadata.Element("Subject");
