71d4996e77c3773bd0f6d8d945c8210097a47cb8
Jesper Fyhr Knudsen <Jesper@fyhr.dk>
2014-04-25 10:29:37 +0200

    renamed change files

--- sql/2.changes/06.00.00.0003.sql
+++ sql/2.changes/06.00.00.0003.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `Metadata` CHANGE COLUMN `MetadataXML` `MetadataXML` MEDIUMTEXT NOT NULL  ;
+
+  INSERT INTO `Version`(`Major`,`Minor`,`Release`,`ScriptName`,`DateCreated`)
+VALUES ('06','00','0003','06.00.0003.sql',NOW());
--- sql/2.changes/06.00.00.0004.sql
+++ sql/2.changes/06.00.00.0004.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `Metadata` 
 CHANGE COLUMN `MetadataXML` `MetadataXML` LONGTEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ;
+
+  INSERT INTO `Version`(`Major`,`Minor`,`Release`,`ScriptName`,`DateCreated`)
+VALUES ('06','00','0004','06.00.0004.sql',NOW());
