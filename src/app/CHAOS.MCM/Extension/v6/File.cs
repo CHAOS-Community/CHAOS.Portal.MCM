b9d688f7cc39c1499515a4064afb5deea4ef2016
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2014-10-07 14:44:46 +0200

    Added full reindex of Object when a file is created or deleted

--- src/app/CHAOS.MCM/Extension/v6/File.cs
+++ src/app/CHAOS.MCM/Extension/v6/File.cs
@@ -7,40 +7,48 @@ using Chaos.Portal.Core.Exceptions;
 
 namespace Chaos.Mcm.Extension.v6
 {
-    public class File : AMcmExtension
+  public class File : AMcmExtension
+  {
+    #region Initialization
+
+    public File(IPortalApplication portalApplication, IMcmRepository mcmRepository, IPermissionManager permissionManager)
+      : base(portalApplication, mcmRepository, permissionManager)
     {
-        #region Initialization
-
-        public File(IPortalApplication portalApplication, IMcmRepository mcmRepository, IPermissionManager permissionManager) : base(portalApplication, mcmRepository, permissionManager)
-        {
-        }
-
-        #endregion
-        #region Business Logic
-
-        public Data.Dto.File Create(Guid objectGuid, uint? parentFileID, uint formatID, uint destinationID, string filename, string originalFilename, string folderPath)
-		{
-            if (!HasPermissionToObject(objectGuid, FolderPermission.CreateUpdateObjects))
-                throw new InsufficientPermissionsException("User does not have permissions to create a file for this object");
-
-            var id     = McmRepository.FileCreate(objectGuid, parentFileID, destinationID, filename, originalFilename, folderPath, formatID);
-            var result = McmRepository.FileGet(id);
-
-            return result;
-		}
-
-		public ScalarResult Delete(uint id )
-		{
-		    var file = McmRepository.FileGet(id);
-            
-            if (!HasPermissionToObject(file.ObjectGuid, FolderPermission.CreateUpdateObjects))
-                throw new InsufficientPermissionsException("User does not have permissions to delete a file on this object");
-
-		    var result = McmRepository.FileDelete(id);
-
-            return new ScalarResult((int)result);
-        }
-
-        #endregion
     }
+
+    #endregion
+
+    #region Business Logic
+
+    public Data.Dto.File Create(Guid objectGuid, uint? parentFileID, uint formatID, uint destinationID, string filename,
+                                string originalFilename, string folderPath)
+    {
+      if (!HasPermissionToObject(objectGuid, FolderPermission.CreateUpdateObjects))
+        throw new InsufficientPermissionsException("User does not have permissions to create a file for this object");
+
+      var id = McmRepository.FileCreate(objectGuid, parentFileID, destinationID, filename, originalFilename, folderPath,
+                                        formatID);
+      var result = McmRepository.FileGet(id);
+
+      ViewManager.Index(McmRepository.ObjectGet(objectGuid, true, true, true ,true, true));
+
+      return result;
+    }
+
+    public ScalarResult Delete(uint id)
+    {
+      var file = McmRepository.FileGet(id);
+
+      if (!HasPermissionToObject(file.ObjectGuid, FolderPermission.CreateUpdateObjects))
+        throw new InsufficientPermissionsException("User does not have permissions to delete a file on this object");
+
+      var result = McmRepository.FileDelete(id);
+
+      ViewManager.Index(McmRepository.ObjectGet(file.ObjectGuid, true, true, true, true, true));
+
+      return new ScalarResult((int) result);
+    }
+
+    #endregion
+  }
 }
