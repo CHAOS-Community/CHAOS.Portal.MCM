38871d13828f7066e9dc555ef0e8f36e1d1f3b0c
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2015-07-07 22:43:05 +0100

    Add Index on GUID and RevsionID. Metadata_Set Throw an Revsion exception if the RevisionID has changed

--- sql/3.storedprocedures/Metadata_Set.sql
+++ sql/3.storedprocedures/Metadata_Set.sql
@@ -7,59 +7,27 @@ CREATE PROCEDURE `Metadata_Set`(
     MetadataXML         MEDIUMTEXT,
     EditingUserGUID     BINARY(16)
 )
-BEGIN
-                        
+BEGIN             
 
-    DECLARE EXIT HANDLER
-    FOR SQLEXCEPTION, SQLWARNING, NOT FOUND
-    BEGIN
-        ROLLBACK;
-        SELECT -200;
-    END;
-
-    START TRANSACTION;
-
-	IF NOT EXISTS(SELECT
-						GUID
-					FROM
-						Metadata
-					WHERE
-						Metadata.GUID = GUID)
-	THEN
-                                  
-        INSERT INTO Metadata
-			( GUID, LanguageCode, MetadataSchemaGUID, MetadataXml, DateCreated, EditingUserGUID )
-        VALUES
-			( GUID, LanguageCode, MetadataSchemaGUID, MetadataXML, NOW()      , EditingUserGUID );
-                
-		INSERT INTO Object_Metadata_Join
+	IF EXISTS(SELECT GUID FROM Metadata WHERE Metadata.GUID = GUID AND Metadata.RevisionID <> RevisionID)	THEN
+	   SELECT -201;
+	ELSE
+		INSERT INTO
+			Metadata (GUID, LanguageCode, MetadataSchemaGUID, MetadataXml, DateCreated, EditingUserGUID) 
+		VALUES 
+			(GUID, LanguageCode, MetadataSchemaGUID, MetadataXML, NOW(), EditingUserGUID)
+		ON DUPLICATE KEY UPDATE 
+			MetadataXml     = MetadataXML, 
+			DateCreated     = NOW(),
+			EditingUserGUID = EditingUserGUID,
+			RevisionID      = Metadata.RevisionID + 1;
+		
+		INSERT IGNORE INTO Object_Metadata_Join
 			(ObjectGuid,MetadataGuid)
 		VALUES
 			(ObjectGUID,GUID);
 
 		SELECT 1;
-
-    ELSE
-
-        UPDATE 
-            Metadata
-        SET 
-            m.MetadataXml     = MetadataXML,
-            m.DateCreated     = NOW(),
-            m.EditingUserGUID = EditingUserGUID,
-            m.RevisionID      = m.RevisionID + 1
-        WHERE 
-				m.GUID = GUID
-			AND m.RevisionID = RevisionID;
-           
-		IF(ROW_COUNT() = 0) then
-			SELECT -201;
-		ELSE
-			SELECT 1;
-		END IF;
-		                        
-    END IF;
-       
-    COMMIT;
+	END IF;
 
 END
