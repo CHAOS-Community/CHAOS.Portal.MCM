9d0b15a37029d5bd6f21d188fbd590cdcfec8a59
Jesper Fyhr Kndusen <jesper@fyhr.dk>
2015-11-22 21:09:03 +0100

    If metadata xml is invalid, then don't return it.

--- src/app/CHAOS.MCM/Data/Mapping/ObjectMetadataMapping.cs
+++ src/app/CHAOS.MCM/Data/Mapping/ObjectMetadataMapping.cs
@@ -1,32 +1,42 @@
+using System.Xml.Linq;
+
 namespace Chaos.Mcm.Data.Mapping
 {
-    using System.Collections.Generic;
-    using System.Data;
+	using System.Collections.Generic;
+	using System.Data;
+	using CHAOS.Data;
+	using Chaos.Mcm.Data.Dto;
 
-    using CHAOS.Data;
+	//todo: eliminate duplication (MetadataMapping)
+	public class ObjectMetadataMapping : IReaderMapping<ObjectMetadata>
+	{
+		public IEnumerable<ObjectMetadata> Map(IDataReader reader)
+		{
+			while (reader.Read())
+			{
+				XDocument metadataXml;
 
-    using Chaos.Mcm.Data.Dto;
+				try
+				{
+					metadataXml = reader.GetXDocument("MetadataXML");
+				}
+				catch (System.Xml.XmlException)
+				{
+					continue;
+				}
 
-    //todo: eliminate duplication (MetadataMapping)
-    public class ObjectMetadataMapping : IReaderMapping<ObjectMetadata>
-    {      
-        public IEnumerable<ObjectMetadata> Map(IDataReader reader)
-        {
-            while(reader.Read())
-            {
-
-                yield return new ObjectMetadata
-                    {
-                        Guid               = reader.GetGuid("Guid"),
-                        ObjectGuid         = reader.GetGuid("ObjectGuid"),
-                        MetadataSchemaGuid = reader.GetGuid("MetadataSchemaGUID"),
-                        RevisionID         = reader.GetUint32("RevisionID"),
-                        MetadataXml        = reader.GetXDocument("MetadataXML"),
-                        DateCreated        = reader.GetDateTime("DateCreated"),
-                        EditingUserGuid    = reader.GetGuid("EditingUserGUID"),
-                        LanguageCode       = reader.GetString("LanguageCode"),
-                    };
-            }
-        }
-    }
+				yield return new ObjectMetadata
+				{
+					Guid = reader.GetGuid("Guid"),
+					ObjectGuid = reader.GetGuid("ObjectGuid"),
+					MetadataSchemaGuid = reader.GetGuid("MetadataSchemaGUID"),
+					RevisionID = reader.GetUint32("RevisionID"),
+					MetadataXml = metadataXml,
+					DateCreated = reader.GetDateTime("DateCreated"),
+					EditingUserGuid = reader.GetGuid("EditingUserGUID"),
+					LanguageCode = reader.GetString("LanguageCode"),
+				};
+			}
+		}
+	}
 }
